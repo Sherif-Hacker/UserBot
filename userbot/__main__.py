9ad3f3b9da6f1f63d45afcfd1e4a5020e97e3436
sʜᴇʀɪғ ʜᴀᴄᴋᴇʀ <117088113+Sherif-Hacker@users.noreply.github.com>
2023-03-31 03:19:02 +0200

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -40,7 +40,7 @@ async def startup_process():
     print("Yay your userbot is officially working.!!!")
     print(
         f"Congratulation, now type {cmdhr}alive to see message if catub is live\
-        \nIf you need assistance, head to https://t.me/catuserbot_support"
+        \nIf you need assistance, head to https://t.me/Sherif_Sami"
     )
     print("============================================================")
     await verifyLoggerGroup()
